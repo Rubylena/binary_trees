c6f1bb16b11aaa9286c78ff3a4505da4c2e602ad
Ruby <graceffiong@gmail.com>
2022-08-25 22:03:30 +0100

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -32,9 +32,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if ((parent->right) != NULL)
 		(parent->right)->parent = newnode;
-
-	newnode->right = parent->right;
-	parent->right = newnode;
-
+	{
+		newnode->right = parent->right;
+		parent->right = newnode;
+	}
 	return (newnode);
 }
