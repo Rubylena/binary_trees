d5bd8e95fe4106463e67799abb89b519ebed16f5
Ruby <graceffiong@gmail.com>
2022-08-25 21:57:32 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	newnod = (struct binary_tree_s *)malloc(sizeof(binary_tree_t));
 
 	if (newnod == NULL)
-		return (0);
+		return (NULL);
 
 	if (parent == NULL)
 		return (NULL);
